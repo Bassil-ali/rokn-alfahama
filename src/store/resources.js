8e1c6bf8c3456fc21467bde8875b86d343d7667b
Ahmed Rafie <ahmedra962015@gmail.com>
2021-12-13 14:25:17 +0200

    update

--- src/store/resources.js
+++ src/store/resources.js
@@ -1,101 +1,114 @@
 let resources = [
     {
-        name:'item',
-        parent:'',
-        discreption:'items resource',
-        headers:['name','code','unit.name','selling_price','quantity','tax.name','created_at']
+        name: 'item',
+        parent: '',
+        discreption: 'items resource',
+        form_route: '/items/form',
+        headers: ['name', 'code', 'unit.name', 'selling_price', 'quantity', 'tax.name', 'created_at']
     },
     {
-        name:'category',
-        parent:'',
-        headers:['name','created_at'],
-        discreption:'categories resource'
+        name: 'category',
+        parent: '',
+        form_route: '/categories/form',
+        headers: ['name', 'created_at'],
+        discreption: 'categories resource'
     },
     {
-        name:'unit',
-        parent:'',
-        headers:['name','created_at'],
-        discreption:'categories resource'
+        name: 'unit',
+        parent: '',
+        form_route: '/units/form',
+        headers: ['name', 'created_at'],
+        discreption: 'categories resource'
     },
     {
-        name:'tax',
-        parent:'',
-        headers:['name','percentage','created_at'],
-        discreption:'categories resource'
+        name: 'tax',
+        parent: '',
+        form_route: '/taxes/form',
+
+        headers: ['name', 'percentage', 'created_at'],
+        discreption: 'categories resource'
     },
     {
-        name:'user',
-        parent:'',
-        headers:['name','user_name','email','mobile','created_at'],
-        discreption:'customers resource'
+        name: 'user',
+        parent: '',
+        form_route: '/users/form',
+
+        headers: ['name', 'user_name', 'email', 'mobile', 'created_at'],
+        discreption: 'customers resource'
     },
     {
-        name:'order',
-        parent:'',
-        headers:['id','user.name','total','discount','tax','taxed_total','status','created_at'],
-        discreption:'orders resource'
+        name: 'order',
+        parent: '',
+        form_route: '/orders/form',
+
+        headers: ['id', 'user.name', 'total', 'discount', 'tax', 'taxed_total', 'status', 'created_at'],
+        discreption: 'orders resource'
     },
     {
-        name:'item',
-        parent:'order',
-        headers:['order_id','item.name','quantity'],
-        discreption:'purchases resource'
+        name: 'item',
+        parent: 'order',
+        headers: ['order_id', 'item.name', 'quantity'],
+        discreption: 'purchases resource'
     },
     {
-        name:'payment',
-        parent:'order',
-        module_name:'order_payment',
-        headers:['order_id','amount','created_at'],
-        discreption:'payments resource'
+        name: 'payment',
+        parent: 'order',
+        form_route:'/payments/form',
+
+        module_name: 'order_payment',
+        headers: ['order_id', 'amount', 'created_at'],
+        discreption: 'payments resource'
     },
     {
-        name:'media',
-        parent:'gallery',
-        discreption:'gallery media resource'
+        name: 'media',
+        parent: 'gallery',
+        discreption: 'gallery media resource'
     },
     {
-        name:'reaction',
-        parent:'item',
-        discreption:'item reaction resource'
+        name: 'reaction',
+        parent: 'item',
+        discreption: 'item reaction resource'
     },
     {
-        name:'setting',
-        parent:'',        
-        discreption:'settings resource'
+        name: 'setting',
+        parent: '',
+        discreption: 'settings resource'
     },
     {
-        name:'media',
-        parent:'',        
-        discreption:'mails resource'
+        name: 'media',
+        parent: '',
+        discreption: 'mails resource'
     },
     {
-        name:'media',
-        parent:'item',        
-        discreption:'mails resource'
+        name: 'media',
+        parent: 'item',
+        discreption: 'mails resource'
     },
     {
-        name:'rank',
-        parent:'item',        
-        discreption:'friends resource'
+        name: 'rank',
+        parent: 'item',
+        discreption: 'friends resource'
     },
     {
-        name:'address',
-        parent:'',        
-        discreption:'address resource'
+        name: 'address',
+        parent: '',
+        discreption: 'address resource'
     },
     {
-        name:'coupon',
-        parent:'',  
-        headers:['id','code','value'],      
-        discreption:'address resource'
+        name: 'coupon',
+        parent: '',
+        form_route:'/coupon/form',
+
+        headers: ['id', 'code', 'value'],
+        discreption: 'address resource'
     },
     {
-        name:'reaction',
-        parent:'item',
+        name: 'reaction',
+        parent: 'item',
     },
     {
-        name:'rank',
-        parent:'item',
+        name: 'rank',
+        parent: 'item',
     }
-] 
+]
 export default resources;
