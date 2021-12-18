f297e2547279acf99b03c13ed2686460535f218f
Bassil-ali <baselali337@gmail.com>
2021-12-18 11:44:33 +0200

    address

--- src/main/views/address_add.vue
+++ src/main/views/address_add.vue
@@ -26,24 +26,24 @@
 
                             <div class="entry-content-myaccount address form">
                                 <h2>{{$t('Add_a_new_site')}}</h2>
-                                <form action="">
+                                <form @submit.prevent="$root.save(item, 'user.address', '/address_add')">
                                     <div class="row">
                                         <div class="col-md-12">
                                             <div class="mb-3">
                                                 <label>{{$t('address_name')}} <span>*</span></label>
-                                                <input type="text" class="form-control" :placeholder="$t('address_name_add')">
+                                                <input type="text" v-model="item.title_name" class="form-control" :placeholder="$t('address_name_add')">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('site')}} <span>*</span></label>
-                                                <input type="text" class="form-control" :placeholder="$t('site_add')">
+                                                <input type="text" v-model="item.area" class="form-control" :placeholder="$t('site_add')">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('widget')}} <span>*</span></label>
-                                                <input type="text" class="form-control" :placeholder="$t('widget_add')">
+                                                <input type="text" v-model="item.widget" class="form-control" :placeholder="$t('widget_add')">
                                             </div>
                                         </div>
                                     </div>
@@ -51,13 +51,13 @@
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('street')}} <span>*</span></label>
-                                                <input type="text" class="form-control" :placeholder="$t('street_add')">
+                                                <input type="text" v-model="item.street" class="form-control" :placeholder="$t('street_add')">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('Avenue')}}</label>
-                                                <input type="text" class="form-control" :placeholder="$t('Avenue_add')">
+                                                <input type="text" v-model="item.avenue" class="form-control" :placeholder="$t('Avenue_add')">
                                             </div>
                                         </div>
                                     </div>
@@ -65,13 +65,13 @@
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('house_number')}} <span>*</span></label>
-                                                <input type="text" class="form-control" :placeholder="$t('house_number_add')">
+                                                <input type="text" v-model="item.house_number" class="form-control" :placeholder="$t('house_number_add')">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('Floor_No')}}</label>
-                                                <input type="text" class="form-control" :placeholder="$t('Floor_No_add')">
+                                                <input type="text" v-model="item.floor_no" class="form-control" :placeholder="$t('Floor_No_add')">
                                             </div>
                                         </div>
                                     </div>
@@ -79,13 +79,13 @@
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('Apartment_number')}}</label>
-                                                <input type="text" class="form-control" :placeholder="$t('Apartment_number_add')">
+                                                <input type="text" v-model="item.apartment_number" class="form-control" :placeholder="$t('Apartment_number_add')">
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label>{{$t('Notes')}}</label>
-                                                <input type="text" class="form-control" :placeholder="$t('Notes_add')">
+                                                <input type="text" v-model="item.notes" class="form-control" :placeholder="$t('Notes_add')">
                                             </div>
                                         </div>
                                     </div>
@@ -103,6 +103,10 @@
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            item: {},
+        }
+    },
 }
 </script>
