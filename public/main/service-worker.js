adda9d160a1e9a33038f52c27851d6834006773a
Ahmed Rafie <ahmedra962015@gmail.com>
2022-01-23 00:28:48 +0200

    update

--- public/main/service-worker.js
+++ public/main/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/main/precache-manifest.8c4de11637860afa6675e39bc216c586.js"
+  "/main/precache-manifest.5591b222e5c2534d7f12c58e26aeee81.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
