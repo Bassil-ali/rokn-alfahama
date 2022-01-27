9beb7911f0accdc2e6c7bb10147208b1911d27da
mythsky-ahmad <sss-s32a@hotmail.com>
2022-01-27 19:37:36 +0200

    test

--- app/Http/Controllers/ItemReactionController.php
+++ app/Http/Controllers/ItemReactionController.php
@@ -12,30 +12,38 @@ use Illuminate\Support\Str;
 
 class ItemReactionController extends BaseController
 {
-    public static function routeName(){
+    public static function routeName()
+    {
         return "item.reaction";
     }
-    public $childRelationName; 
+    public $childRelationName;
     public function __construct(Request $request)
     {
         parent::__construct($request);
         $this->childRelationName = "reactions";
     }
-    public function index(Request $request,Item $item)
+    public function index(Request $request, Item $item)
     {
         $childRelationName = $this->childRelationName;
-        return ReactionResource::collection($item->$childRelationName()->search($request)->sort($request)->paginate((request('per_page')??request('itemsPerPage'))??15));
+        return ReactionResource::collection($item->$childRelationName()->search($request)->sort($request)->paginate((request('per_page') ?? request('itemsPerPage')) ?? 15));
     }
     public function store(Request $request, Item $item)
     {
-        if(!$this->user->is_permitted_to('store',Reaction::class,$request))
-            return response()->json(['message'=>'not_permitted'],422);
+        if (!$this->user->is_permitted_to('store', Reaction::class, $request))
+            return response()->json(['message' => 'not_permitted'], 422);
 
-        $validator = Validator::make($request->all(),Reaction::createRules($this->user));
-        if($validator->fails()){
-            return response()->json(['errors'=>$validator->errors()],422);
+        $validator = Validator::make($request->all(), Reaction::createRules($this->user));
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
-        $reaction = Reaction::create($validator->validated());
+        $reaction_id =  Reaction::where('item_id', $validator->validated()['item_id'])->where('user_id', $validator->validated()['user_id'])->pluck('id');
+        if (count($reaction_id) > 0) {
+            $reaction = Reaction::destroy($reaction_id);
+            return $reaction;
+        } else {
+            $reaction = Reaction::create($validator->validated());
+        }
+        // $reaction = Reaction::create($validator->validated());
         $childRelationName = $this->childRelationName;
         $item->$childRelationName()->save($reaction);
         if ($request->translations) {
@@ -44,19 +52,19 @@ class ItemReactionController extends BaseController
         }
         return new ReactionResource($reaction);
     }
-    public function show(Request $request,Item $item, Reaction $reaction)
+    public function show(Request $request, Item $item, Reaction $reaction)
     {
-        if(!$this->user->is_permitted_to('view',Reaction::class,$request))
-            return response()->json(['message'=>'not_permitted'],422);
+        if (!$this->user->is_permitted_to('view', Reaction::class, $request))
+            return response()->json(['message' => 'not_permitted'], 422);
         return new ReactionResource($reaction);
     }
     public function update(Request $request, Item $item, Reaction $reaction)
     {
-        if(!$this->user->is_permitted_to('update',Reaction::class,$request))
-            return response()->json(['message'=>'not_permitted'],422);
-        $validator = Validator::make($request->all(),Reaction::updateRules($this->user));
-        if($validator->fails()){
-            return response()->json(['errors'=>$validator->errors()],422);
+        if (!$this->user->is_permitted_to('update', Reaction::class, $request))
+            return response()->json(['message' => 'not_permitted'], 422);
+        $validator = Validator::make($request->all(), Reaction::updateRules($this->user));
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
         $reaction->update($validator->validated());
         if ($request->translations) {
@@ -64,13 +72,12 @@ class ItemReactionController extends BaseController
                 $reaction->setTranslation($translation['field'], $translation['locale'], $translation['value'])->save();
         }
         return new ReactionResource($reaction);
-
     }
 
-    public function destroy(Request $request,Item $item, Reaction $reaction)
+    public function destroy(Request $request, Item $item, Reaction $reaction)
     {
-        if(!$this->user->is_permitted_to('delete',Reaction::class,$request))
-            return response()->json(['message'=>'not_permitted'],422);
+        if (!$this->user->is_permitted_to('delete', Reaction::class, $request))
+            return response()->json(['message' => 'not_permitted'], 422);
         $reaction->delete();
         return new ReactionResource($reaction);
     }
