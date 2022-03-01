6430e4188a5cc3d4f162b86e3147e6f6ee74f623
Bassil-ali <baselali337@gmail.com>
2022-03-01 14:20:06 +0200

    deploy

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -88,7 +88,7 @@ class AuthController extends BaseController
         return response()->json([
             'access_token' => $token,
             'token_type' =>  'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => auth()->factory()->getTTL() * 60*7,
             'user' => auth()->user()
         ]);
     }
